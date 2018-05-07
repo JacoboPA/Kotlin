2efef9783c17f15893250b150a15ccd937b63481
Unknown <jacobopa58@gmail.com>
2018-05-07 19:11:45 +0200

    cambios

--- PrimerProgramaKotlin/src/seccion3/When.kt
+++ PrimerProgramaKotlin/src/seccion3/When.kt
@@ -7,6 +7,8 @@ fun main(args : Array<String>){
         1-> println("el valor vale 1 ")
         2-> println("vale 2")
         in 9..12 -> println("valor entre 9 y 12")
+
+        
         else -> println("está fuera")
     }
 }
